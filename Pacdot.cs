75095349858e6f5b5af6ee7bc140e766e94ebfad
107360236 <43467444+107360236@users.noreply.github.com>
2019-01-03 22:13:14 +0800

    Add files via upload

---
+++ Pacdot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//觸發檢測器,銷毀豆子
+	void OnTriggerEnter2D(Collider2D co)
+	{
+		if (co.name == "pacman")
+			Destroy (gameObject);
+	}
+}
